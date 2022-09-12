7ca58f2cf92543f2aa89a575123bff3e1160b446
David - Adriel <david@adriel.solutions>
2022-09-12 09:49:05 +0300

    Added upgrade command

--- classes/constants.php
+++ classes/constants.php
@@ -4,4 +4,5 @@
 
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
+        public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
     }
