59ba70928a9c0790ef288639299d15ac5b234181
David - Adriel <david@adriel.solutions>
2022-09-12 10:58:59 +0300

    Added support for looping via iterator

--- classes/commands.php
+++ classes/commands.php
@@ -3,9 +3,11 @@
     namespace classes;
 
     use ArrayAccess;
+    use Iterator;
 
-    class Commands implements ArrayAccess {
+    class Commands implements ArrayAccess, Iterator {
         private array $arr;
+        private int $position = 0;
 
         public function __construct() {
             $this->arr = array();
@@ -33,5 +35,30 @@
         public function offsetGet($offset) : Command
         {
             return isset($this->arr[$offset]) ? $this->arr[$offset] : null;
+        }
+
+        public function rewind() : void
+        {
+            reset($this->arr);
+        }
+
+        public function current() : mixed
+        {
+            return current($this->arr);
+        }
+
+        public function key() : mixed
+        {
+            return key($this->arr);
+        }
+
+        public function next() : void
+        {
+            next($this->arr);
+        }
+
+        public function valid() : bool
+        {
+            return key($this->arr) !== null;
         }
     }
