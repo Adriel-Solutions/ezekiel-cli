00a35e0173614b396e9e0bb7413979c48e6f9654
David - Adriel <david@adriel.solutions>
2022-09-19 09:26:30 +0300

    Implemented install:ui command, and removed skeleton for upgrade:ui

--- commands/install/ui.php
+++ commands/install/ui.php
@@ -6,52 +6,26 @@
     use classes\command;
     use classes\constants;
 
-    class ui extends command {
+    class Ui extends command {
         protected string $identifier = 'install:ui';
         protected string $description = 'install the remote ui library inside the project by downloading its code';
         protected string $help = 'usage: ezekiel install:ui';
-        protected array $dependencies = [ 'git' , 'sed' , 'awk' , 'grep' ];
+        protected array $dependencies = [ 'git' ];
 
         public function run(application $app, ?string ...$args): void
         {
             $this->exit_if_missing_dependencies($app);
 
-            if(empty($args)) {
-                $app->output('Missing arguments');
-                $app->output('Usage: ezekiel install:module <name>');
+            if(file_exists("./app/views/components/input.php")) {
+                $app->output("The user interface is installed already");
+                $app->output("Upgrade with: ezekiel upgrade:ui");
                 return;
             }
 
-            $name = $args[0];
+            $app->execute('git clone ' . Constants::$GIT_ORGANIZATION_URL . '/ezekiel-ui.git ./app');
+            $app->execute('rm -rf ./app/.git');
 
-            if(file_exists("./app/modules/$name/module.json")) {
-                $metadata = json_decode(file_get_contents("./app/modules/$name/module.json"), true);
-                $version = $metadata['version'];
-                $app->output("Module $name installed already in version $version");
-                return;
-            }
-
-            $repository_url = Constants::$GIT_ORGANIZATION_URL . "/ezekiel-module-$name";
-            $app->execute('git ls-remote ' . $repository_url . ' 2>/dev/null');
-            $repository_exists = 0 === $app->get_last_exit_code();
-
-            if(!$repository_exists) {
-                $app->output("Module $name does not exist");
-                return;
-            }
-
-            $app->execute("git clone $repository_url ./app/modules/$name");
-            $app->execute("rm -rf ./app/modules/$name/.git");
-
-            $line_opening = (int) $app->execute('cat ./app/bootstrap.php | grep -n "public static function setup" | grep -o "^[0-9]*"');
-            $line_closing = $line_opening + (int) $app->execute('sed -n \'6,$p\' ./app/bootstrap.php | grep -nm 1 "}" | grep -o "^[0-9]*"');
-
-            $instruction = "\t\t\tmodule(\"$name\")->activate();";
-            $app->execute('sed -i "" -e $\'' . $line_closing - 2 . ' a\\\\\\n\'\'' . $instruction . '\' ./app/bootstrap.php');
-
-            $app->ezekiel('run:migrations');
-
-            $app->output("Module $name installed and activated");
+            $app->output("The user interface library was installed");
         }
     }
 
