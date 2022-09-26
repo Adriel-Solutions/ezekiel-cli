9936c1e1fdb2e54f61d2c5bfc8201893e5769e30
David - Adriel <david@adriel.solutions>
2022-09-26 21:00:31 +0200

    Fixed typo

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.50";
-        public static $VERSION_COMMIT = "70e8702";
+        public static $VERSION_NUMBER = "1.0.51";
+        public static $VERSION_COMMIT = "132b068";
         public static $VERSION_DATE = "2022-09-26";
     }
