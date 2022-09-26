e00ae133832ba2e98f2fe2ca970705d4e6188bf0
David - Adriel <david@adriel.solutions>
2022-09-26 19:52:03 +0200

    Updated hook to generate version

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "";
-        public static $VERSION_COMMIT = "";
-        public static $VERSION_DATE = "";
+        public static $VERSION_NUMBER = "1.0.48";
+        public static $VERSION_COMMIT = "62e2767";
+        public static $VERSION_DATE = "2022-09-26";
     }
