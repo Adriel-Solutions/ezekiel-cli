70e87027b51b64871c89cef1987c425bd9e948d3
David - Adriel <david@adriel.solutions>
2022-09-26 20:56:15 +0200

    Added docker:ezekiel

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.48";
-        public static $VERSION_COMMIT = "62e2767";
+        public static $VERSION_NUMBER = "1.0.49";
+        public static $VERSION_COMMIT = "e00ae13";
         public static $VERSION_DATE = "2022-09-26";
     }
