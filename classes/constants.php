11bd5b6838d73a691fedc3ed6123a94825bf6e73
David - Adriel <david@adriel.solutions>
2022-10-10 18:41:23 +0300

    Added still wip docker:sessions implementation

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.64";
-        public static $VERSION_COMMIT = "2c4ea9f";
+        public static $VERSION_NUMBER = "1.0.65";
+        public static $VERSION_COMMIT = "6b89c63";
         public static $VERSION_DATE = "2022-10-10";
     }
--- commands/docker/sessions.php
+++ commands/docker/sessions.php
@@ -12,12 +12,66 @@
         protected string $help = 'Usage: ezekiel docker:sessions';
         protected array $dependencies = [ 'docker' ];
 
+        private function _to_long_type(string $short_type) {
+            return [
+                'a' => 'array',
+                'b' => 'bool',
+                's' => 'string',
+                'o' => 'object',
+                'i' => 'int',
+                'f' => 'float'
+            ][$short_type];
+        }
+
         public function run(Application $app, ?string ...$args): void
         {
             $this->exit_if_missing_dependencies($app);
             $container = $app->execute("cat docker-compose.dev.yml | grep -E \"container_name: .*-fpm\" | sed -E 's/container_name://' | tr -d '\" '");
-            $output = $app->execute("docker exec -it $container sh -c 'cat /tmp/sess*'");
-            $app->output($output);
+            $command = 'find /tmp/ -name sess* -print | xargs -I{} sh -c "cat {} && echo"';
+            $output = $app->execute("docker exec -it $container sh -c '$command'");
+
+            if(empty($output)) exit();
+    
+            // Multi-line to array
+            $sessions = explode("\n", $output);
+
+            // Removing last ; of each line
+            $sessions = array_map(fn($s) => rtrim($s, ';'), $sessions);
+
+            // Turning every line into a proper PHP array representation
+            $sessions = array_map(
+                function($s) {
+                    $attributes = explode(';', $s);
+                    $attributes = array_map(
+                        function($a) {
+                            $first_parts = explode('|', $a);
+                            $second_parts = explode(':', $first_parts[1]);
+
+                            return [
+                                'name' => $first_parts[0],
+                                'type' => $this->_to_long_type($second_parts[0]),
+                                'length' => $second_parts[0] !== 's' ? 'NA' : $second_parts[1],
+                                'value' => $second_parts[0] === 's' ? trim($second_parts[2], '"') : $second_parts[1]
+                            ];
+                        },
+                        $attributes
+                    );
+                    return $attributes;
+                },
+                $sessions
+            );
+
+            // Turn attributes into a simple string \n-delimited
+            $sessions = array_map(
+                fn($s) => implode(" / ", array_map( fn($a) => $a['name'] . ' (' . $a['type'] . ') = ' . $a['value'] , $s)),
+                $sessions
+            );
+
+            // Print table
+            $headers = [ 'ID' , 'SESSION' ];
+            $rows = []; for($i = 0; $i < count($sessions); $i++) $rows[] = [ $i + 1 , $sessions[$i] ];
+
+            $app->output_table($headers, $rows);
         }
     }
 
