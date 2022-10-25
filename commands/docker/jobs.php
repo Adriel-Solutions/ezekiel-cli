2fa4828866497de84c7eb540d0dcdfbf0cd29da3
David - Adriel <david@adriel.solutions>
2022-10-25 11:43:41 +0300

    WIP docker:jobs

--- commands/docker/jobs.php
+++ commands/docker/jobs.php
@@ -21,7 +21,7 @@
                 require './native/index.php';
                 $service = default_service('jobs');
                 $jobs = $service->get_all([ 'order' => [ 'pk' => 'ASC' ] ]);
-                var_dump($jobs);
+                print(json_encode($jobs));
             EOF;
 
             $handle = tmpfile();
