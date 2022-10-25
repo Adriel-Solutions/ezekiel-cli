3e9d151b034472055f9fab84663b547308c41981
David - Adriel <david@adriel.solutions>
2022-10-25 13:11:47 +0300

    Fixed quote

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.87";
-        public static $VERSION_COMMIT = "2d2c4c1";
+        public static $VERSION_NUMBER = "1.0.88";
+        public static $VERSION_COMMIT = "a3d1049";
         public static $VERSION_DATE = "2022-10-25";
     }
