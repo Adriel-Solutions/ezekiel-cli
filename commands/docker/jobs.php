be4d91e36cede1439d926c022ec2013ace74048a
David - Adriel <david@adriel.solutions>
2022-10-25 11:23:00 +0300

    WIP docker:jobs

--- commands/docker/jobs.php
+++ commands/docker/jobs.php
@@ -15,8 +15,23 @@
         {
 
             $this->exit_if_missing_dependencies($app);
-            $container = $app->execute("cat docker-compose.dev.yml | grep -E \"container_name: .*-db\" | sed -E 's/container_name://' | tr -d '\" '");
-            $output = $app->execute("docker exec -it --user postgres $container psql -d project -c 'SELECT * FROM jobs'");
+
+            $script = <<<'EOF'
+                require './native/index.php';
+                $service = default_service('jobs');
+                $jobs = $service->get_all([ 'order' => [ 'pk' => 'ASC' ] ]);
+                var_dump($jobs);
+            EOF;
+
+            $handle = tmpfile();
+            $handle_uri = stream_get_meta_data($handle)['uri'];
+            $handle_filename = pathinfo($handle_uri, PATHINFO_FILENAME);
+            fwrite($handle, $script);
+            fclose($handle);
+
+            $container = $app->execute("cat docker-compose.dev.yml | grep -E \"container_name: .*-fpm\" | sed -E 's/container_name://' | tr -d '\" '");
+            $app->execute("docker cp $handle_uri $container:/tmp/");
+            $output = $app->execute("docker exec -w /app -it $container sh -c 'php -f /tmp/$handle_filename'");
             var_dump($output);
             /* $app->output_table( */
             /*     [ 'Job' , 'Status' , 'Last run at' ], */
