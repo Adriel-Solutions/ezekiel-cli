768d640227d022077a041aec005dc2f5dd7b7f70
David - Adriel <david@adriel.solutions>
2022-10-25 11:55:46 +0300

    Added docker:job command

--- classes/constants.php
+++ classes/constants.php
@@ -6,6 +6,6 @@
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
         public static $VERSION_NUMBER = "1.0.82";
-        public static $VERSION_COMMIT = "17bee5c";
+        public static $VERSION_COMMIT = "9ecbb8b";
         public static $VERSION_DATE = "2022-10-25";
     }
--- commands/docker/job.php
+++ commands/docker/job.php
@@ -6,22 +6,31 @@
     use classes\Application;
 
     class Jobs extends Command {
-        protected string $identifier = 'docker:jobs';
-        protected string $description = 'List all the pending and executed jobs';
-        protected string $help = 'Usage: ezekiel docker:jobs';
+        protected string $identifier = 'docker:job';
+        protected string $description = 'Show all the variables of a job';
+        protected string $help = 'Usage: ezekiel docker:job <ID>';
         protected array $dependencies = [ 'docker' , 'sed' , 'tr' , 'grep' ];
 
         public function run(Application $app, ?string ...$args): void
         {
-
             $this->exit_if_missing_dependencies($app);
+
+            if(empty($args)) {
+                $app->output('Missing arguments');
+                $app->output('Usage: ezekiel docker:job <ID>');
+                return;
+            }
+
+            $id = $args[0];
 
             $script = <<<'EOF'
                 <?php
                 require './native/index.php';
                 $service = default_service('jobs');
-                $jobs = $service->get_all([ 'order' => [ 'pk' => 'ASC' ] ]);
-                print(json_encode($jobs));
+            EOF
+                .'$job = $service->get(' . $id . ')'
+                .<<<'EOF'
+                print(json_encode($job));
             EOF;
 
             $handle = tmpfile();
@@ -38,21 +47,11 @@
             $app->execute("docker cp $new_handle_uri $container:/tmp/$handle_filename");
             $output = $app->execute("docker exec -w /app -it $container sh -c 'php -f /tmp/$handle_filename'");
 
-            $jobs = json_decode($output, true);
+            $job = json_decode($output, true);
             $app->output_table(
-                [ 'ID' , 'Job' , 'Running' , 'For' , 'Frequency' , 'From' ,'Last' ],
-                array_map(
-                    fn($j) => [
-                        $j['pk'] ,
-                        $j['class'],
-                        $j['is_running'] ? 'Yes' : 'No',
-                        $j['scheduled_for'] ?? 'None',
-                        $j['schedule_frequency'] ?? 'None',
-                        $j['scheduled_from'] ?? 'None',
-                        $j['last_run_at'] ?? 'None',
-                    ],
-                    $jobs,
-                )
+                array_keys($job),
+                array_values($job)
             );
         }
     }
+
