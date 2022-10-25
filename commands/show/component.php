deadac967b629da4d4165bd3d705462c73dfb052
David - Adriel <david@adriel.solutions>
2022-10-25 18:28:24 +0300

    Added WIP show:component

--- commands/show/component.php
+++ commands/show/component.php
@@ -23,9 +23,28 @@
 
             $name = strtolower($args[0]);
 
-            $raw = $app->execute("cat native/views/components/$name.php | tr '\n' ' ' | grep '<?php .* Parameters .*\*\/' --color -o");
-            $lines = explode(' ', $raw);
-            var_dump($lines);
+            $content = file_get_contents("native/views/components/$name.php");
+            $lines = explode("\n", $content);
+
+            $relevant_lines = [];
+            $block_started = false;
+            foreach($lines as $l) {
+                if(str_contains($l, '* Parameters :')) {
+                    $block_started = true;
+                    continue;
+                }
+
+                if(!$block_started)
+                    continue;
+
+                if($block_started && str_contains($l, '?>'))
+                    break;
+
+                if(str_contains($l, '* - '))
+                    $relevant_lines[] = $l;
+            }
+
+            var_dump($relevant_lines);
         }
     }
 
