e309740cf5269e376ad5e63b62ec548da855d824
David - Adriel <david@adriel.solutions>
2022-10-25 11:04:13 +0300

    Added WIP docker:jobs command

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.67";
-        public static $VERSION_COMMIT = "da2ec51";
+        public static $VERSION_NUMBER = "1.0.68";
+        public static $VERSION_COMMIT = "57cb98d";
         public static $VERSION_DATE = "2022-10-25";
     }
--- commands/docker/jobs.php
+++ commands/docker/jobs.php
@@ -1,18 +1,23 @@
 <?php
 
-    namespace commands\show;
+    namespace commands\docker;
 
     use classes\Command;
     use classes\Application;
 
     class Jobs extends Command {
-        protected string $identifier = 'show:jobs';
+        protected string $identifier = 'docker:jobs';
         protected string $description = 'List all the pending and executed jobs';
-        protected string $help = 'Usage: ezekiel show:jobs';
+        protected string $help = 'Usage: ezekiel docker:jobs';
+        protected array $dependencies = [ 'docker' , 'sed' , 'tr' , 'grep' ];
 
         public function run(Application $app, ?string ...$args): void
         {
 
+            $this->exit_if_missing_dependencies($app);
+            $container = $app->execute("cat docker-compose.dev.yml | grep -E \"container_name: .*-db\" | sed -E 's/container_name://' | tr -d '\" '");
+            $output = $app->execute("docker exec -it --user postgres $container psql -c 'SELECT * FROM jobs'");
+            var_dump($output);
             /* $app->output_table( */
             /*     [ 'Job' , 'Status' , 'Last run at' ], */
             /*     array_map( */
@@ -22,4 +27,5 @@
             /* ); */
         }
     }
+
 
