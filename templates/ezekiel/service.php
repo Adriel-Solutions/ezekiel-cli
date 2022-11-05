f6bec3f2fd24ff7e96e810af9f1a19d9417a1932
David - Adriel <david@adriel.solutions>
2022-11-05 19:23:18 +0200

    Fixed missing type hint

--- templates/ezekiel/service.php
+++ templates/ezekiel/service.php
@@ -5,7 +5,7 @@
     use native\facades\Service;
 
     class <PLURAL> extends Service {
-        protected string $table = "<TABLE>";
+        protected ?string $table = "<TABLE>";
         protected array $relations = [];
         protected array $schema = [];
     }
