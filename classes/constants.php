c4ffab66a33e42360d57cc651bc0a8099658991d
David - Adriel <david@adriel.solutions>
2022-12-07 00:22:23 +0100

    Replaced tailwind with bundle

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.104";
-        public static $VERSION_COMMIT = "56be4f7";
-        public static $VERSION_DATE = "2022-12-06";
+        public static $VERSION_NUMBER = "1.0.105";
+        public static $VERSION_COMMIT = "596ec36";
+        public static $VERSION_DATE = "2022-12-07";
     }
--- commands/run/bundle.php
+++ commands/run/bundle.php
@@ -6,16 +6,22 @@
     use classes\Command;
     use classes\Constants;
 
-    class Tailwind extends Command {
-        protected string $identifier = 'run:tailwind';
-        protected string $description = 'Run Tailwind build';
-        protected string $help = 'Usage: ezekiel run:tailwind';
+    class Bundle extends Command {
+        protected string $identifier = 'run:bundle';
+        protected string $description = 'Run Tailwind/Alpine build';
+        protected string $help = 'Usage: ezekiel run:bundle';
         protected array $dependencies = [ 'tailwind' ];
 
         public function run(Application $app, ?string ...$args): void
         {
             $this->exit_if_missing_dependencies($app);
+
+            // Tailwind
             $output = $app->execute("tailwind -i ./native/views/assets/styles/style.min.css -o ./native/views/assets/styles/tw.min.css");
+            $app->output($output);
+
+            // Alpine
+            $output = $app->execute("find ./app/ ./native/views/assets/scripts/components/ -type f -name \"*.js\" -exec cat {} \; > native/views/assets/scripts/bundle.min.js");
             $app->output($output);
         }
     }
