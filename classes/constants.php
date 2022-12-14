6e8b17b1f05477ba1439877e1d46dac9ef3b5f7e
David - Adriel <david@adriel.solutions>
2022-12-14 22:41:22 +0100

    Added missing dot

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.111";
-        public static $VERSION_COMMIT = "8951a89";
+        public static $VERSION_NUMBER = "1.0.112";
+        public static $VERSION_COMMIT = "34ead5c";
         public static $VERSION_DATE = "2022-12-14";
     }
