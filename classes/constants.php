66205ddd0b3f7d1bd59a9331420b3fa49da97ed2
David - Adriel <david@adriel.solutions>
2023-01-01 13:10:42 +0100

    Added show:routes command

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.113";
-        public static $VERSION_COMMIT = "6e8b17b";
-        public static $VERSION_DATE = "2022-12-29";
+        public static $VERSION_NUMBER = "1.0.114";
+        public static $VERSION_COMMIT = "5c0e8ba";
+        public static $VERSION_DATE = "2023-01-01";
     }
---
+++ commands/show/routes.php
@@ -0,0 +1,56 @@
+<?php
+
+    namespace commands\show;
+
+    use classes\Command;
+    use classes\Application;
+
+    class Routes extends Command {
+        protected string $identifier = 'show:routes';
+        protected string $description = 'List all the routes';
+        protected string $help = 'Usage: ezekiel show:routes';
+
+        public function run(Application $app, ?string ...$args): void
+        {
+            $this->exit_if_missing_dependencies($app);
+
+            $script = $app->fill_template('/scripts/docker/jobs.php', []);
+            $output = $app->run_script($script);
+
+            $routes = json_decode($output, true);
+
+            if(!$app->is_csv_enforced())
+                $app->output_with_pagination(
+                    $app->to_table(
+                        // Method | Url | Controller | Gateways (middlewares)
+                        [ 'M', 'U' , 'C' ],
+                        array_map(
+                            fn($r) => [
+                                $r['method'],
+                                $r['url'],
+                                (!empty($r['module']) ? '(' . $r['module'] . ')' : '' ) .$r['controller'] . '->' . $r['function'],
+                            ],
+                            $routes,
+                        )
+                    )
+                );
+            else
+                $app->output_csv(
+                    // Method | Url | Controller | Gateways (middlewares)
+                    [ 'Method', 'Url' ,  'Module' , 'Controller' ,  'Function', 'Middlewares' ],
+                    array_map(
+                        fn($r) => [
+                            $r['method'],
+                            $r['url'],
+                            empty($r['module']) ? 'None' : $r['module'],
+                            empty($r['controller']) ? 'None' : $r['controller'],
+                            $r['function'],
+                            join(', ', $r['middlewares'])
+                        ],
+                        $routes,
+                    )
+                );
+        }
+    }
+
+
