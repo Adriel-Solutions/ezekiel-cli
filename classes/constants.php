4c0e51a435d2f12870baba20d0328fd3a1f42806
David - Adriel <david@adriel.solutions>
2023-01-14 10:25:17 +0100

    Added cssnano

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.115";
-        public static $VERSION_COMMIT = "66205dd";
-        public static $VERSION_DATE = "2023-01-01";
+        public static $VERSION_NUMBER = "1.0.116";
+        public static $VERSION_COMMIT = "e2a2246";
+        public static $VERSION_DATE = "2023-01-14";
     }
--- commands/run/bundle.php
+++ commands/run/bundle.php
@@ -10,15 +10,18 @@
         protected string $identifier = 'run:bundle';
         protected string $description = 'Run Tailwind/Alpine build';
         protected string $help = 'Usage: ezekiel run:bundle';
-        protected array $dependencies = [ 'tailwind' , 'uglifyjs' ];
+        protected array $dependencies = [ 'tailwind' , 'uglifyjs' , 'cssnano' ];
 
         public function run(Application $app, ?string ...$args): void
         {
             $this->exit_if_missing_dependencies($app);
 
             // Tailwind
-            $output = $app->execute("tailwind -i ./native/views/assets/styles/style.min.css -o ./native/views/assets/styles/tw.min.css");
+            $output = $app->execute("tailwind -i ./native/views/assets/styles/style.min.css -o ./native/views/assets/styles/tw.css");
             $app->output($output);
+
+            // CSSNano
+            $output = $app->execute("cssnano < ./native/views/assets/styles/tw.css > ./native/views/assets/styles/tw.min.css");
 
             // Alpine scripts - App
             $output = $app->execute("find ./app/ -type f -name \"*[^min].js\" -print | xargs -I {} echo {} | sed 's/...$//' | xargs -I {} uglifyjs {}.js --output {}.min.js");
