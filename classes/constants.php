75b127236a0476fdd725b83c58643ea1a38ce34d
David - Adriel <david@adriel.solutions>
2023-01-15 12:17:47 +0100

    Updated CSS processing in run:bundle to include vendor-prefixing and better minification

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.116";
-        public static $VERSION_COMMIT = "e2a2246";
-        public static $VERSION_DATE = "2023-01-14";
+        public static $VERSION_NUMBER = "1.0.117";
+        public static $VERSION_COMMIT = "4c0e51a";
+        public static $VERSION_DATE = "2023-01-15";
     }
--- commands/run/bundle.php
+++ commands/run/bundle.php
@@ -10,7 +10,7 @@
         protected string $identifier = 'run:bundle';
         protected string $description = 'Run Tailwind/Alpine build';
         protected string $help = 'Usage: ezekiel run:bundle';
-        protected array $dependencies = [ 'tailwind' , 'uglifyjs' , 'cssnano' ];
+        protected array $dependencies = [ 'tailwind' , 'uglifyjs' , 'lightningcss' ];
 
         public function run(Application $app, ?string ...$args): void
         {
@@ -20,8 +20,8 @@
             $output = $app->execute("tailwind -i ./native/views/assets/styles/style.min.css -o ./native/views/assets/styles/tw.css");
             $app->output($output);
 
-            // CSSNano
-            $output = $app->execute("cssnano < ./native/views/assets/styles/tw.css > ./native/views/assets/styles/tw.min.css");
+            // Lightningcss
+            $output = $app->execute("lightningcss --minify --bundle --targets 'cover 99.5%' ./native/views/assets/styles/tw.css > ./native/views/assets/styles/tw.min.css");
 
             // Alpine scripts - App
             $output = $app->execute("find ./app/ -type f -name \"*[^min].js\" -print | xargs -I {} echo {} | sed 's/...$//' | xargs -I {} uglifyjs {}.js --output {}.min.js");
