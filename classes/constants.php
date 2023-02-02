65e61cdc5a0f235c6d11c98b1edbbee57db899a7
David - Adriel <david@adriel.solutions>
2023-02-02 19:34:37 +0200

    Removed trash dir

--- classes/constants.php
+++ classes/constants.php
@@ -5,7 +5,7 @@
     class Constants {
         public static $GIT_ORGANIZATION_URL = "https://github.com/Adriel-Solutions";
         public static $GIT_REPOSITORY_URL = "https://github.com/Adriel-Solutions/ezekiel";
-        public static $VERSION_NUMBER = "1.0.119";
-        public static $VERSION_COMMIT = "6ee0b21";
+        public static $VERSION_NUMBER = "1.0.120";
+        public static $VERSION_COMMIT = "d987134";
         public static $VERSION_DATE = "2023-02-02";
     }
